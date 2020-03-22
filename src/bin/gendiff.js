6d1815cca8cd8631f7df58ab0479bf6d0411850c
MammaSonnim <msonnim@gmail.com>
2020-03-22 20:43:15 +0300

    :tada: init and 1st step - show help

---
+++ src/bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import program from 'commander';
+import { version } from '../../package.json'
+
+program
+  .arguments('')
+  .version(version)
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
